e39bd15ce4aa906713c2542eb576458f660e65aa
AndreyKerges <andrey.nascimento4@etec.sp.gov.br>
2023-04-07 23:16:51 -0300

    exercício resolvido

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace teste_de_num_impares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 5;
+            while(numero <= 125)
+            {
+                Console.WriteLine(numero);
+                numero += 2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
